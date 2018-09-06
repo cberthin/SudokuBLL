89f4203dea0beca747c8b780f12425ef2a837010
ChristianBerthin <ChristianBerthin@wifi-roaming-128-4-83-153.host.udel.edu>
2018-09-06 11:40:30 -0400

    Added Contains Zero Method

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -2,5 +2,30 @@ package pkgGame;
 
 public class Sudoku {
 
+	public boolean ContainsZero();
+	{
+		for (int iCol=0;iCol<LatinSquare.length; iCol++)
+		{
+			for )int iRow=0; iRow<LatinSquare.length; iRow++)
+				{
+					if(LatinSquare[iCol][iRow]==0)
+					{
+						return true;
+					}
+				}
+		}
+		return false;
+	}
 	private int[][] puzzle;
+	{
+		
+	}
+	private int[][] getLatinSquare();
+	{
+		
+	}
+	private void setLatinSquare(int[][] latinSquare);
+	{
+		
+	}
 }
